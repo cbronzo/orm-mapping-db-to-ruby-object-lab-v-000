f539dcfd64249824e6db22719bd367f7ffc33f4d
Catherine Bronzo <cbronzo@mac.com>
2018-07-23 14:26:27 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL
-       SELECT * FROM students WHERE students.grade => 12
+       SELECT * FROM students WHERE students.grade < 12 
        SQL
 
        DB[:conn].execute(sql)
